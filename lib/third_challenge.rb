6ba8f33f0ac678583cd939ebdd216cef065a3c5a
premti <premti@gmail.com>
2020-02-20 16:29:47 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,7 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        name: "", age:
+        name: "Lord C", age:
         },
       :matriarch => {
         #your key/value pairs here

68df3f6339df696ad8122ccbc14bc7cbc1169b07
premti <premti@gmail.com>
2020-02-20 16:28:20 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,7 +5,7 @@ def third_challenge
         name: "Lord Montague" , age: "53"
         },
       :matriarch => {
-        [:name, :age]
+        name: "", age
         },
       :hero => {
         [:name, :age]
